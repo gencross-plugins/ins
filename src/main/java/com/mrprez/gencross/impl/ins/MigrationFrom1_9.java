22a94c840f3e929ac65b1b18730209b1ec55046c
MrPrez <gagneret@yahoo.fr>
2014-09-16 01:44:40 +0200

    Correct migration

---
+++ src/main/java/com/mrprez/gencross/impl/ins/MigrationFrom1_9.java
@@ -0,0 +1,15 @@
+package com.mrprez.gencross.impl.ins;
+
+import com.mrprez.gencross.Version;
+import com.mrprez.gencross.migration.MigrationPersonnage;
+import com.mrprez.gencross.migration.Migrator;
+
+public class MigrationFrom1_9 implements Migrator {
+
+	@Override
+	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1,11));
+		return migrationPersonnage;
+	}
+
+}
